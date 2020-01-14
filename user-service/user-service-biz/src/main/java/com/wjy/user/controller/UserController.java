5695df10cd43a95f2d63d39e8bc208086fbd465d
wjy <978621447@qq.com>
2020-01-14 18:06:29 +0800

    user模块划分 pom结构修改

--- user-service/user-service-biz/src/main/java/com/wjy/user/controller/UserController.java
+++ user-service/user-service-biz/src/main/java/com/wjy/user/controller/UserController.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.RestController;
 /**
  * @author WangJinYi 2020/1/12
  */
-@RequestMapping("/user")
+@RequestMapping("/com/wjy/user")
 @RestController
 public class UserController {
 
